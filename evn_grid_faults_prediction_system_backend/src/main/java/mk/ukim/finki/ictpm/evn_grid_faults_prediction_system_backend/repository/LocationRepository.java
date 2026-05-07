32fe418760cf44d69a99d961faba2902b738a06e
Dimitar Trposki <dimitar.curry30@gmail.com>
2026-05-07 23:04:14 +0200

    Customer module

--- evn_grid_faults_prediction_system_backend/src/main/java/mk/ukim/finki/ictpm/evn_grid_faults_prediction_system_backend/repository/LocationRepository.java
+++ evn_grid_faults_prediction_system_backend/src/main/java/mk/ukim/finki/ictpm/evn_grid_faults_prediction_system_backend/repository/LocationRepository.java
@@ -4,6 +4,8 @@ import mk.ukim.finki.ictpm.evn_grid_faults_prediction_system_backend.model.domai
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 }
